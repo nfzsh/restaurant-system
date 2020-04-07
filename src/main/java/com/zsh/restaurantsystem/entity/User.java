ef8de9e0027a04207fffd6d90a3a6a02276041d0
祝世豪 <zsh199803@hotmail.com>
2020-04-08 02:11:09 +0800

    entity user test

---
+++ src/main/java/com/zsh/restaurantsystem/entity/User.java
@@ -0,0 +1,42 @@
+package com.zsh.restaurantsystem.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int openid;
+    private String name;
+    private LocalDate birth;
+    private String phone_num;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+            updatable = false,
+            insertable = false)
+    private LocalDateTime insertTime;
+
+    public User(int openid) {
+        this.openid = openid;
+    }
+
+    public User(int openid, String name, LocalDate birth,
+                String phone_num) {
+        this.openid = openid;
+        this.name = name;
+        this.birth = birth;
+        this.phone_num = phone_num;
+    }
+}
--- src/test/java/com/zsh/restaurantsystem/RestaurantSystemApplicationTests.java
+++ src/test/java/com/zsh/restaurantsystem/RestaurantSystemApplicationTests.java
@@ -1,13 +1,28 @@
 package com.zsh.restaurantsystem;
 
+import com.zsh.restaurantsystem.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.Month;
 
 @SpringBootTest
+@Transactional
+@Rollback(value = false)
 class RestaurantSystemApplicationTests {
 
+    @PersistenceContext
+    private EntityManager em;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        var birth = LocalDate.of(1998, Month.MARCH,8);
+        User user = new User(123,"zsh",birth,"4008823823");
+        em.persist(user);
     }
 
 }
