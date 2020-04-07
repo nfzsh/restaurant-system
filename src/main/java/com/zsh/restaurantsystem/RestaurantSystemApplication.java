d3bfb19e853710912c7d1407bd5d47a8f5e8336f
祝世豪 <zsh199803@hotmail.com>
2020-04-07 19:43:05 +0800

    create

---
+++ src/main/java/com/zsh/restaurantsystem/RestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zsh.restaurantsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsh/restaurantsystem/RestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsh.restaurantsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
