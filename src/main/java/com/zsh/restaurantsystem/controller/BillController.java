9f2bc2a6f1d42f33855ffe073feb2c285f86ed36
祝世豪 <zsh199803@hotmail.com>
2020-04-24 20:47:52 +0800

    bill api

--- src/main/java/com/zsh/restaurantsystem/controller/BillController.java
+++ src/main/java/com/zsh/restaurantsystem/controller/BillController.java
@@ -1,13 +1,13 @@
 package com.zsh.restaurantsystem.controller;
 
 import com.zsh.restaurantsystem.entity.Bill;
+import com.zsh.restaurantsystem.entity.User;
 import com.zsh.restaurantsystem.service.BillService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @Slf4j
@@ -20,5 +20,23 @@ public class BillController {
     @PostMapping("/add")
     public void setBill(@RequestBody Bill bill){
         billService.setBill(bill);
+    }
+    @PostMapping("/update")
+    public void updateBill(@RequestBody Bill bill){
+        billService.setBill(bill);
+    }
+    //user.id
+    @PostMapping("/select_uid")
+    public Map selectBill(@RequestBody User user){
+        return Map.of("bill",billService.getBillByUid(user));
+    }
+    //bill.id
+    @PostMapping("/select_id")
+    public Map selectBill(@RequestBody Bill bill){
+        return Map.of("bill",billService.getBill(bill));
+    }
+    @GetMapping("/select_all")
+    public Map selectAll(){
+        return Map.of("bills",billService.getAllBill());
     }
 }
--- src/main/java/com/zsh/restaurantsystem/service/BillService.java
+++ src/main/java/com/zsh/restaurantsystem/service/BillService.java
@@ -1,12 +1,14 @@
 package com.zsh.restaurantsystem.service;
 
 import com.zsh.restaurantsystem.entity.Bill;
+import com.zsh.restaurantsystem.entity.User;
 import com.zsh.restaurantsystem.repository.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,8 +16,24 @@ public class BillService {
     @Autowired
     BillRepository billRepository;
 
-    public Bill setBill(Bill bill){
+    public Bill setBill(Bill bill) {
         billRepository.save(bill);
         return billRepository.refresh(bill);
+    }
+
+    public void deleteBill(Bill bill) {
+        billRepository.deleteById(bill.getId());
+    }
+
+    public Optional<Bill> getBill(Bill bill) {
+        return billRepository.findById(bill.getId());
+    }
+
+    public List<Bill> getBillByUid(User user) {
+        return billRepository.getBill(user.getId());
+    }
+
+    public List<Bill> getAllBill() {
+        return billRepository.findAll();
     }
 }
