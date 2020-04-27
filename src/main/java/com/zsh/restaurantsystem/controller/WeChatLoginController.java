fed4c611caea8183f403fdf27abcc8ae43111add
祝世豪 <zsh199803@hotmail.com>
2020-04-27 23:55:50 +0800

    import

--- src/main/java/com/zsh/restaurantsystem/controller/WeChatLoginController.java
+++ src/main/java/com/zsh/restaurantsystem/controller/WeChatLoginController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/wechat")
