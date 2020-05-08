6c55fde50c4ae1d2111af62272d6197591bd16d6
nfzsh <zsh199803@hotmail.com>
2020-05-08 21:15:22 +0800

    API fixed

--- src/main/java/com/zsh/restaurantsystem/controller/TablesController.java
+++ src/main/java/com/zsh/restaurantsystem/controller/TablesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -18,23 +19,24 @@ public class TablesController {
     TablesService tablesService;
 
     @PostMapping("/add")
-    public Map setTable(@RequestBody Tables tables, HttpServletResponse response) {
-        //1保存成功0重复no
-        return Map.of("message",tablesService.setTables(tables));
+    public Map setTable(@RequestBody Tables tables) {
+        tablesService.setTables(tables);
+        return Map.of("tables",tablesService.getAll());
     }
 
-    @PostMapping("/delete")
-    public void deleteTable(@RequestBody Tables tables, HttpServletResponse response) {
-        tablesService.deleteTables(tables.getId());
+    @GetMapping("/delete/{tid}")
+    public Map deleteTable(@PathVariable int tid, HttpServletResponse response) {
+        tablesService.deleteTables(tid);
+        return Map.of("tables",tablesService.getAll());
     }
 
     @PostMapping("/update")
-    public Map updateTable(@RequestBody Tables tables, HttpServletResponse response) {
+    public Map updateTable(@RequestBody Tables tables) {
         if (tables.getId()==0)
-            return Map.of("message",0);
+            return Map.of("tables",tablesService.getAll(),"message","更新失败");
         else {
             tablesService.updateTables(tables);
-            return Map.of("message",1);
+            return Map.of("tables",tablesService.getAll(),"message","更新成功");
         }
     }
 
